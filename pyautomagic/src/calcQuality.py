2c1d342c98b959e777e82ae77f27cb1a92b5975d
Adam Li <adam2392@gmail.com>
2019-09-22 15:36:33 -0400

    Update calcQuality.py
    Updating your code with mainly stylistic changes:
    1. variable naming: ideally you're super explicit, so ppl have an idea what the variable is w/o needing to search for your abbreviation, or worse interpret. 
    -> change the metrics to be something explicit like MAV = mean_abs_voltage
    
    2. commenting: If you're using pycharm, you can just initialize docstrings pretty easily like this. """""", then enter and it'll make them for you.
    
    3. misc.: 
    - you can get all variables set by shape instantly.
    - most of the time, print statements are fine, but Python has this very very helpful thing called logger, which helps you output log files when you're debugging vs. running a program. Sounded like the user will get a log of their final output, so I'm just centralizing the flow of these messages into a module built for handling this kinda thing. Rn, you'll still see the message print out (in std.out aka your terminal), but later on we can redirect them to a log file essentially with 1-2 lines of code.

--- pyautomagic/src/calcQuality.py
+++ pyautomagic/src/calcQuality.py
@@ -1,57 +1,84 @@
-def calcQuality(Dat,bad_chans,overallThresh=50,timeThresh=25,chanThresh=25,avRef=True):
-    """ Calculates four essential quality metrics of a data set
-    INPUTS: Dat, a channels x timepoints data array of EEG
-            bad_chans, a list of the numbers of bad channels
-            overallThresh, timeThresh, and chanThresh are thresholds for 3 of the metrics
-            avRef, boolean for if average referencing should be applied
-    OUTPUTS: quality_metrics, a dictionary of the quality metrics and thresholds/settings used
-    The four quality metrics:
-        Overall high amplitude data points
-        Timepoints of high variance across channels
-        Ratio of bad channels
-        Channels of high variance across time
-        
-        Mean absolute voltage also calculated
-    """
-    import numpy as np
-    #checking all of the default value types, if not what they should be, use default
-    if not isinstance(overallThresh,int) and not isinstance(overallThresh,float):
-        overallThresh = 50
-        print('Invalid overallThresh value. Default of 50 used.')
-    if not isinstance(timeThresh,int) and not isinstance(timeThresh,float):
-        timeThresh = 25
-        print('Invalid timeThresh value. Default of 25 used.')
-    if not isinstance(chanThresh,int) and not isinstance(chanThresh,float):
-        chanThresh = 25
-        print('Invalid chanThresh value. Default of 25 used.')
-    if not isinstance(avRef,bool):
-        # ADD STATEMENT TO ALLOW TYPICAL OTHER BOOLEAN INDICATORS
-        avRef = True
-        print('Invalid avRef value. Average referencing used.')
-    # ADD CHECKS FOR DAT AND BAD_CHANS?
-        
-    dimensions = np.shape(Dat)#tuple of EEG data info
-    n_chans = dimensions[0]
-    n_timepts = dimensions[1]
-    n_elements = dimensions[0]*dimensions[1]#total number data points
-    # perform average reference
-    if avRef:
-        av = np.mean(Dat,0)
-        Dat = Dat - np.tile(av,(n_chans,1))
-    # Calculating quality metrics
-    # Overall high amplitude data points
-    OHA = np.sum(np.absolute(Dat)>overallThresh)/n_elements
-    # Timepoints of high variance
-    std_across_chans = np.std(Dat,0)
-    THV = np.sum(std_across_chans>timeThresh)/n_timepts
-    # Ratio of bad channels
-    RBC = len(bad_chans)/n_chans
-    # Channels of high variance
-    std_across_time = np.std(Dat,1)
-    CHV = np.sum(std_across_time>chanThresh)/n_chans
-    # unthresholded mean absolute voltage
-    MAV = np.mean(np.absolute(Dat))
-    quality_metrics = {'OHA':OHA,'THV':THV,'RBC':RBC,'CHV':CHV,'MAV':MAV,#actual results
-                       'ot':overallThresh,'tt':timeThresh,'ct':chanThresh,'ar':avRef}#for development
-    return quality_metrics
+from typing import List
+import logging
+import numpy as np
 
+logger = logging.getLogger(__name__)
+
+class Quality():
+    def calcQuality(self, Data: np.ndarray, bad_chans: List, overallThresh: float = 50, timeThresh: float = 25,
+                    chanThresh: float = 25, apply_common_avg: bool = True):
+        """
+        Calculates four essential quality metrics of a data set and returns a dictionary of the quality metrics and thresholds/settings used
+
+        The four quality metrics:
+            Overall high amplitude data points
+            Timepoints of high variance across channels
+            Ratio of bad channels
+            Channels of high variance across time
+
+        Mean absolute voltage is also calculated.
+
+        :param Data: a channels x timepoints data array of EEG
+        :type Data: np.ndarray
+        :param bad_chans:  a list of the numbers of bad channels
+        :type bad_chans: list
+        :param overallThresh: overall threshold for rejection of ...
+        :type overallThresh: float
+        :param timeThresh: threshold for rejecting time segments
+        :type timeThresh: float
+        :param chanThresh: threshold for rejecting channels
+        :type chanThresh: float
+        :param apply_common_avg: if average referencing should be applied
+        :type apply_common_avg: bool
+        :return: quality_metrics: a dictionary of the quality metrics and thresholds/settings used
+            The four quality metrics:
+            Overall high amplitude data points
+            Timepoints of high variance across channels
+            Ratio of bad channels
+            Channels of high variance across time
+
+            Mean absolute voltage also calculated
+        :rtype: dict
+        """
+        # checking all of the default value types, if not what they should be, use default
+        if not isinstance(overallThresh, int) and not isinstance(overallThresh, float):
+            overallThresh = 50
+            logger.log('Invalid overallThresh value. Default of 50 used.')
+        if not isinstance(timeThresh, int) and not isinstance(timeThresh, float):
+            timeThresh = 25
+            logger.log('Invalid timeThresh value. Default of 25 used.')
+        if not isinstance(chanThresh, int) and not isinstance(chanThresh, float):
+            chanThresh = 25
+            logger.log('Invalid chanThresh value. Default of 25 used.')
+        if not isinstance(apply_common_avg, bool):
+            # ADD STATEMENT TO ALLOW TYPICAL OTHER BOOLEAN INDICATORS
+            apply_common_avg = True
+            logger.log('Invalid apply_common_avg value. Average referencing used.')
+        # ADD CHECKS FOR DAT AND BAD_CHANS?
+
+        # get the dimensions of EEG data (# channels, # time points, # samples of data)
+        n_chans, n_times, n_samples = Data.shape
+
+        # perform average reference
+        if apply_common_avg:
+            avg_signal = np.mean(Data, axis=0)
+            Data = Data - avg_signal
+
+        # Calculating quality metrics
+        # Overall high amplitude data points
+        OHA = np.sum(np.absolute(Data) > overallThresh) / n_samples
+        # Timepoints of high variance
+        std_across_chans = np.std(Data, axis=0)
+        THV = np.sum(std_across_chans > timeThresh) / n_times
+        # Ratio of bad channels
+        RBC = len(bad_chans) / float(n_chans)
+        # Channels of high variance
+        std_across_time = np.std(Data, axis=1)
+        CHV = np.sum(std_across_time > chanThresh) / n_chans
+        # unthresholded mean absolute voltage
+        MAV = np.mean(np.absolute(Data))
+
+        quality_metrics = {'OHA': OHA, 'THV': THV, 'RBC': RBC, 'CHV': CHV, 'MAV': MAV,  # actual results
+                           'ot': overallThresh, 'tt': timeThresh, 'ct': chanThresh, # for development
+                           'ar': apply_common_avg}  # for development
+        return quality_metrics
