0c424a264f5eb865a9a03a3badbc3beec2615be0
deepsoni1996 <54995736+deepsoni1996@users.noreply.github.com>
2019-09-29 23:54:22 -0400

    Added perform_high_variance_channel_rejection.py code

---
+++ pyautomagic/preprocessing/perform_high_variance_channel_rejection.py
@@ -0,0 +1,45 @@
+import logging
+import numpy as np
+logger = logging.getLogger(__name__)
+
+
+def perform_high_variance_channel_rejection(data: np.ndarray, removed_mask: np.array(dtype=bool),
+                                            sd_threshold: float = 25):
+    """
+    reject bad channels based on high standard deviation
+
+    rejected = perform_high_variance_channel_rejection(data, removed_mask, sd_threshold)
+
+    Here rejected is a list of channels that must be removed. data and removed_mask will be part of EEG_in structure.
+    sd_threshold is an optional input for standard deviation threshold. When it is omitted, default value is used.
+
+    :param data: data array of EEG
+    :param removed_mask: mask of removed channels
+    :param sd_threshold: threshold value for standard deviation
+    :return: data_out : EEG data after channel rejection
+    """
+
+    # checking input arguments and if not acceptable, assigning default value.
+    if not isinstance(sd_threshold, int) and not isinstance(sd_threshold, float):
+        sd_threshold = 0.85
+        logger.log('Invalid channel_criterion value. Default of 0.85 used.')
+
+    # dimension of EEG data
+    s, _ = data.shape
+
+    # initializing bad_channel_mask
+    bad_channels_mask = np.zeros((1, s), dtype=bool)
+
+    # calculating standard deviation and rejecting channels with higher sd than threshold value
+    rejected = np.nanstd(data, axis=1) > sd_threshold
+    data_out = data[np.where(rejected)[0]] = 0
+
+    # updating other parameters of dataset accordingly
+    bad_channels_mask[rejected] = True
+    new_mask = removed_mask
+    old_mask = removed_mask
+    new_mask[np.where(new_mask=0)[0]] = bad_channels_mask
+    bad_channels = np.setdiff1d(np.where(new_mask)[0], np.where(old_mask))
+    removed_mask = new_mask
+
+    return data_out, bad_channels, removed_mask
