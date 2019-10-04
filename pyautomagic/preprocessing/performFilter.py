c1fd9a448e6b8ef51282771c50b0e3754dd3f49f
Nick3151 <xiangliang3151@gmail.com>
2019-10-04 16:53:17 -0400

    Update performFilter.py in preprocessing

--- pyautomagic/preprocessing/performFilter.py
+++ pyautomagic/preprocessing/performFilter.py
@@ -1,61 +1,60 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 27 20:42:02 2019
-
-@author: Vector
-"""
 import mne
 import numpy as np
+import logging
 
-def performFilter(EEG, sfreq, filter_type=None, cutoff_freq=None, filter_length='auto'):
-    """This function filters EEG data using Hamming windowed sinc FIR filter
+
+def performFilter(EEG, sfreq, filter_type=None, filt_freq=None, filter_length='auto'):
+    """ This function filters EEG data using Hamming windowed sinc FIR filter
     with input filter type and parameters.
-    Inputs: 
-        EEG                 -ndarray, shape (…, n_times)
-        sfreq               -sample frequency
-        filter_type         -filter types, can only take 'low', 'high' or 'notch'
-        cutoff_freq         -cut-off frequency of filter(Hz)
-        filter_length       -Length of the FIR filter to use
-        
-    Outputs:
-        The filtered data  -ndarray, shape (…, n_times)"""
+
+    :param EEG: Input EEG data to be filtered.
+    :type EEG: ndarray, shape (…, n_times)
+    :param sfreq: The sample frequency in Hz.
+    :type sfreq: float | None
+    :param filter_type: The filter type, can only take 'low', 'high' or 'notch', defaults to None
+    :type filter_type: str
+    :param filt_freq: The filter frequency in Hz, defaults to None
+    :type filt_freq: float | None
+    :param filter_length: Length of the FIR filter to use, defaults to 'auto'.
+    :type filter_length: str | int
+    :return: The filtered EEG data.
+    :rtype: ndarray, shape (…, n_times)
+    """
     if filter_type is None:
-        print('No Filter Will be Performed')
-        if cutoff_freq is not None:
-            print('Warning: Unused filter parameter cutoff_freq')
+        logging.warning('No Filter Will be Performed')
+        if filt_freq is not None:
+            logging.warning('Unused filter parameter filt_freq')
+        EEG_filt = EEG
     elif filter_type not in ('low', 'high', 'notch'):
-        print("Error input format: filter_type must be 'low', 'high' or 'notch'")
+        logging.error("filter_type must be 'low', 'high' or 'notch'")
     else:
         if filter_type == 'low':
-            if cutoff_freq is None:
-                print('Warning: cutoff freq is not given but is required. Default parameters' 
-                              'for low pass filtering will be used')
-                cutoff_freq = 30    #Default
-            EEG_filt = mne.filter.filter_data(EEG, sfreq, None, cutoff_freq, 
-                                  picks=None, filter_length=filter_length, l_trans_bandwidth='auto', 
-                                  h_trans_bandwidth='auto', n_jobs=1, method='fir', 
-                                  phase='zero', fir_window='hamming', 
-                                  fir_design='firwin', pad='reflect_limited', verbose=None)
+            if filt_freq is None:
+                logging.warning('Upper pass-band freq is not given but is required. Default parameters' 
+                                'for low pass filtering will be used')
+                filt_freq = 30    # Default
+            EEG_filt = mne.filter.filter_data(EEG, sfreq, None, filt_freq, picks=None,
+                                              filter_length=filter_length, l_trans_bandwidth='auto',
+                                              h_trans_bandwidth='auto', n_jobs=1, method='fir',
+                                              phase='zero', fir_window='hamming',
+                                              fir_design='firwin', pad='reflect_limited', verbose=None)
         if filter_type == 'high':
-            if cutoff_freq is None:
-                print('Warning: cutoff freq is not given but is required. Default parameters'  
-                              'for high pass filtering will be used')
-                cutoff_freq = 0.5    #Default
-            EEG_filt = mne.filter.filter_data(EEG, sfreq, cutoff_freq, None, 
-                                  picks=None, filter_length=filter_length, l_trans_bandwidth='auto', 
-                                  h_trans_bandwidth='auto', n_jobs=1, method='fir', 
-                                  phase='zero', fir_window='hamming', 
-                                  fir_design='firwin', pad='reflect_limited', verbose=None)
+            if filt_freq is None:
+                logging.warning('Lower pass-band freq is not given but is required. Default parameters'
+                                'for high pass filtering will be used')
+                filt_freq = 0.5    # Default
+            EEG_filt = mne.filter.filter_data(EEG, sfreq, filt_freq, None,
+                                              picks=None, filter_length=filter_length, l_trans_bandwidth='auto',
+                                              h_trans_bandwidth='auto', n_jobs=1, method='fir',
+                                              phase='zero', fir_window='hamming',
+                                              fir_design='firwin', pad='reflect_limited', verbose=None)
         if filter_type == 'notch':
-            if cutoff_freq is None:
-                print('Warning: cutoff freq for notch filter is not complete.'
-                              'The default will be used.')
-                cutoff_freq = 50    #Default
-            EEG_filt = mne.filter.notch_filter(EEG, sfreq, cutoff_freq, 
-                                  filter_length=filter_length, notch_widths=None, trans_bandwidth=1, 
-                                  method='fir', phase='zero', fir_window='hamming', fir_design='firwin', 
-                                  pad='reflect_limited', verbose=None)
+            if filt_freq is None:
+                logging.warning('Frequency for notch filter is not complete.'
+                                'The default will be used.')
+                filt_freq = 60    # Default
+            EEG_filt = mne.filter.notch_filter(EEG, sfreq, filt_freq,
+                                               filter_length=filter_length, notch_widths=None, trans_bandwidth=1,
+                                               method='fir', phase='zero', fir_window='hamming', fir_design='firwin',
+                                               pad='reflect_limited', verbose=None)
     return EEG_filt
-            
-                
-        
