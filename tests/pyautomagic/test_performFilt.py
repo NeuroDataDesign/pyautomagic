307613790771f6dbfd14db812e0c80d40ab66d25
Nick3151 <xiangliang3151@gmail.com>
2019-10-05 19:08:57 -0400

    update test_performFilter.py

--- tests/pyautomagic/test_performFilt.py
+++ tests/pyautomagic/test_performFilt.py
@@ -1,49 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 24 20:06:37 2019
-
-@author: Vector
-"""
-
 import mne
-import os
 import numpy as np
-import matplotlib.pyplot as plt
-
-test_dir = os.path.abspath(os.path.dirname(os.getcwd()))
-testdata_dir = os.path.join(test_dir,'test_data','scalp_test.edf')
-pyautomagic_dir = os.path.dirname(test_dir)
-
-import sys
-sys.path.append(pyautomagic_dir)
+import pytest
 from pyautomagic.preprocessing import performFilter
 
-raw = mne.io.read_raw_edf(testdata_dir, preload=True)
-#raw.plot()
 
-#Filter data using Hamming windowed sinc FIR filter
-EEG_raw = raw._data  #EEG data in ndarray(...,n_times)
-sfreq = raw.info['sfreq']  #sampling frequency
-times = raw.times  
-EEG_lowpass_filt = performFilter.performFilter(EEG_raw, sfreq, 'low')
-EEG_highpass_filt = performFilter.performFilter(EEG_raw, sfreq, 'high')
-EEG_notch_filt = performFilter.performFilter(EEG_raw, sfreq, 'notch', 60)
-
-fig, axs = plt.subplots(2,figsize=(20,10))
-fig.suptitle('Filtering EEG data(Notch)')
-axs[0].plot(times, EEG_raw[0])
-axs[1].plot(times, EEG_notch_filt[0])
-plt.xlabel('time(s)')
-
-plt.figure(figsize=(20,10))
-plt.subplot(2,1,1)
-plt.specgram(EEG_raw[0],Fs=sfreq)
-cb = plt.colorbar()
-cb.set_label('PSD Intensity(dB)')
-plt.ylabel('Frequency(Hz)')
-plt.subplot(2,1,2)
-plt.specgram(EEG_notch_filt[0],Fs=sfreq)
-cb = plt.colorbar()
-cb.set_label('PSD Intensity(dB)')
-plt.xlabel('time(s)')
-plt.ylabel('Frequency(Hz)')
+times = np.arange(0,.001,20)
+low_freq_signal = 2*np.sin(2*np.pi*10*times)
+high_freq_signal = np.cos(2*np.pi*80*times)
+power_freq_noise = 5*np.sin(2*np.pi*60*times)
+white_noise = np.random.normal(0.5,size=np.size(times))
