5d9be6e277dcdf8fb37862c973c20af172c02d12
rbechto2 <54995036+rbechto2@users.noreply.github.com>
2019-10-16 10:37:02 -0400

    Made proper varible names and numpy docstring comments and added more test functions for performRPCA

--- pyautomagic/preprocessing/rpca/performRPCA.py
+++ pyautomagic/preprocessing/rpca/performRPCA.py
@@ -1,31 +1,39 @@
 import numpy as np
-import pyautomagic.preprocessing.rpca.preProcessRPCA as preprocess
-
-""" Perform Robust Principle Component Analysis:
-    -Input = [EEG,params]
-           EEG = EEG Data (must include)
-           lam = Lamda paramter for RPCA (default = 1/(sqrt(# of Colunms))
-           tol = Tolerance (defalut = 1e-7) RPCA param
-           maxIter = Maximum Iterations (deafult = 1000)
-    
-    -Output = [Data, Noise]
-            Data = Corrected Data
-            Noise = Noise removed from the data
-            Original EEG = Data + Noise        
-    
-    -Summary = Performs a Robust Principal Component Analysis on the EEG data with
-            the specified parameters: Lamda, Tolerance, and Maximum number of Iterations.
-            The function outputs the EEG data with the noise removed as well as the nosie
-            that was removed.
-"""
-
+import preProcessRPCA as preprocess
 
 def performRPCA(EEG, lam=-1, tol=1e-7, maxIter=1000):
-    # Find lamda if not provided using the Automagic algorithim
-    col = EEG.shape
-    if lam == -1:  # if no input lamda, calculate its value
-        lam = 1 / np.sqrt(col[0])
 
-    # Perform Robust Principal Component Analysis
-    data, error = preprocess.rpca(EEG, lam, tol, maxIter)
-    return data, error
+    """ Perform Robust Principle Component Analysis:
+
+    Performs a Robust Principal Component Analysis on the EEG data with
+    the specified parameters: Lamda, Tolerance, and Maximum number of Iterations.
+    The function outputs the EEG data with the noise removed as well as the nosie
+    that was removed.
+    
+    Parameters
+    ----------
+    EEG : double numpy array
+        First Parameter, EEG Data (must include)
+    lam : double (default = 1/(sqrt(# of Colunms))
+        Second Parameter, lamda paramter for RPCA (input -1 for default calculation)
+    tol : double (defalut = 1e-7)
+        Third Parameter, tolerance RPCA parameter
+    maxIter : int
+        Fourth Parameter, Maximum Iterations (deafult = 1000)
+    
+    Returns
+    -------
+    Data : double numpy array
+        Corrected Data
+    Noise : double numpy array
+        Noise removed from the data, Original EEG = Data + Noise        
+    
+    """
+    #Find lamda if not provided using the Automagic algorithim
+    col = EEG.shape;
+    if (lam == -1): #if no input lamda, calculate its value
+        lam = 1 / np.sqrt(col[0]);
+        
+    #Perform Robust Principal Component Analysis
+    data, error = preprocess.rpca(EEG,lam,tol,maxIter);
+    return data, error;
