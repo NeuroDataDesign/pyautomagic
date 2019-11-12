35f95d408e1d1d1d39fa3f7999448e60eb75371f
clairezurn <czurn1@jhmi.edu>
2019-11-12 09:45:21 -0500

    finish draft of Block

--- pyautomagic/src/Block.py
+++ pyautomagic/src/Block.py
@@ -5,9 +5,14 @@ import os
 #import re
 import json
 #from pyautomagic.src.calcQuality import calcQuality
-
+import mne
 from mne_bids.utils import _parse_bids_filename, _write_json
 from mne_bids.read import _read_raw
+from pyautomagic.preprocess.preprocess import preprocess, interpolate
+from pyautomagic.src.calcQuality import calcQuality
+from pyautomagic.src.rateQuality import rateQuality
+from matplotlib import pyplot as plt
+
 
 logger = logging.getLogger(__name__)
 
@@ -64,6 +69,8 @@ class Block():
         self.sampling_rate = project.sampling_rate
         self.root_path = root_path
         self.result_path = self.find_result_path
+        self = self.update_rating_from_file
+        self.visualization_params = project.visualization_params
         
     
     def update_rating_from_file(self):
@@ -146,13 +153,27 @@ class Block():
             
         """
         data = self.load_data
-        # do some parameter checks
-        # use externally written function to preprocess
-        # calcQuality
-        # update self.Quality
-        # store a bunch of stuff into an automagic object for results file
-        # save files
-        # write log
+        preprocessed,fig_1,fig_2 = preprocess(data,self.params)
+        quality_scores = calcQuality(preprocessed,self.final_bad_chans,self.project.quality_thresholds)
+        automagic = preprocessed.pop('automagic')
+        update_to_be_stored = {'rate':'not rated','is_manually_rated':False,
+                               'to_be_interpolated':automagic['auto_bad_chans'],
+                               'final_bad_chans':[],'is_interpolated':False,
+                               'quality_scores':quality_scores,'commit':True}
+        self.update_rating(update_to_be_stored)
+        automagic.update({'to_be_interpolated':automagic.auto_bad_chans,
+                          'final_bad_chans':self.final_bad_chans,
+                          'version':self.project.config.version,
+                          'quality_scores':self.quality_scores,
+                          'quality_thresholds':self.project.quality_thresholds,
+                          'rate':self.rate,
+                          'is_manually_rated':self.is_manually_rated,
+                          'times_commited':self.times_commited})
+        results = {'preprocessed':preprocessed,'automagic':automagic}
+        self.save_all_files(results,fig_1,fig_2)
+        self.write_log(results)
+        return results
+    
     def load_data(self):
         """
         Load raw data from BIDS folder
@@ -193,13 +214,41 @@ class Block():
         none
         """
         # update can have many fields, go through and see what they are and update the block accordingly
-
-    def save_all_files(self,fig1,fig2):
+        if 'quality_scores' in update:
+            self.quality_scores = update.quality_scores
+        if 'rate' in update:
+            self.rate = update['rate']
+            if not self.rate == 'interpolate' and not 'to_be_interpolated' in update:
+                self.to_be_interpolated = []
+        if 'is_manually_rated' in update:
+            this_rate = rateQuality(self.quality_scores,self.project.rate_cutoffs)
+            if update['is_manually_rated'] and not update['rate'] == this_rate:
+                self.is_manually_rated = True
+            elif not update['is_manually_rated']:
+                self.is_manually_rated = False
+        if 'to_be_interpolated' in update:
+            self.to_be_interpolated = update['to_be_interpolated']
+            if not update['to_be_interpolated'] == []:
+                self.rate = 'interpolate'
+        if 'final_bad_chans' in update:
+            if update['final_bad_chans'] == []:
+                self.final_bad_chans = update['final_bad_chans']
+            else:
+                self.final_bad_chans.extend(update['final_bad_chans'])
+        if 'is_interpolated' in update:
+            self.is_interpolated = update['is_interpolated']
+        if 'commit' in update and update['commit'] == True:
+            self.times_committed += 1
+            
+        self.project.update_rating_list(self)
+    def save_all_files(self,results,fig1,fig2):
         """
         Save results dictionary and figures to results path
         
         Parameters
         ----------
+        results:
+            MNE raw object with info attribute containing 
         fig1:
             Figure of ??
         
@@ -211,14 +260,24 @@ class Block():
         none
         
         """
-        main_result_file = self.create_results_file
+        main_result_file = results.info['automagic']
         result_filename = self.unique_name + '_results.json'
         result_file_overall = os.path.join(self.result_path,result_filename)
-        
-        # save the processed stuff and figures into results path
-        # dict_to_save = vars(self)
         _write_json(result_file_overall,main_result_file,overwrite=True,verbose = True)
-        # save figs, store processed eeg - could add to above dict
+        
+        processed_filename = self.unique_name+'_raw.fif'
+        processed_file_overall = os.path.join(self.result_path,processed_filename)
+        results.save(processed_file_overall,overwrite=True)
+
+        plt.figure(fig1.number)
+        fig1_name = self.unique_name + '.png'
+        fig1_name_overall = os.path.join(self.result_path,fig1_name)
+        plt.savefig(fig1_name_overall,dpi=200)
+        plt.figure(fig2.number)
+        fig2_name = self.unique_name + '_orig.png'
+        fig2_name_overall = os.path.join(self.result_path,fig2_name)
+        plt.savefig(fig2_name_overall,dpi=100)
+        
     def write_log(self,updates):
         """
         Writes a log for all of the updates its making/actions performed
@@ -231,7 +290,10 @@ class Block():
         log object???
         
         """
-        
+        logger.info(f'pyautomagic version {self.config[''version'']}')
+        logger.info(f'Project:{self.project.name}, Subject:{self.subject.name}, File: {self.uniquename}')
+        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        # TODO: log more things from the preprocessing
     def interpolate(self):
         """
         Interpolates bad channels to create new data and updates info
@@ -245,33 +307,48 @@ class Block():
         none
         
         """
+        result_filename = self.unique_name + '_results.json'
+        result_file_overall = os.path.join(self.result_path,result_filename)
+        if os.path.isfile(result_file_overall):
+            with open(result_file_overall) as json_file:
+                automagic = json.load(json_file)
+        processed_filename = self.unique_name+'_raw.fif'
+        processed_file_overall = os.path.join(self.result_path,processed_filename)
+        eeg = _read_raw(processed_file_overall)
+        interpolate_chans = self.to_be_interpolated
+        if interpolate_chans ==[]:
+            raise(ValueError, 'The subject is rated to be interpolated but no channels chosen')
+            return
+        if self.params == [] or not 'interpolation_params' in self.params or self.params['interpolation_params'] == []:
+            default_params = self.config['default_params']
+            interpolation_params = default_params['interpolation_params']
+        else:
+            interpolation_params = self.params['interpolation_params']
+        interpolated = interpolate(eeg,interpolate_chans,interpolation_params['method'])
+        
+        quality_scores = calcQuality(preprocessed,self.final_bad_chans,self.project.quality_thresholds)
+        update_to_be_stored = {'rate':'not rated','is_manually_rated':False,
+                               'to_be_interpolated':[],
+                               'final_bad_chans':interpolate_chans,
+                               'is_interpolated':True,
+                               'quality_scores':quality_scores}
+        self.update_rating(update_to_be_stored)
+        automagic.update({'interpolation':{'channels':interpolate_chans,
+                                           'params': interpolation_params},
+                          'quality_scores':self.quality_scores,
+                          'rate':self.rate})
+        results = {'preprocessed':interpolated,'automagic':automagic}
+        self.write_log(automagic)
+        automagic.update({'to_be_interpolated':self.to_be_interpolated,
+                          'rate':self.rate,
+                          'auto_bad_chans':self.auto_bad_chans,
+                          'quality_scores':self.quality_scores,
+                          'rate':self.rate})
         # load up the preprocessed results file from results location
         # perform the interpolation (uses some EEGLab stuff I believe)
         # calcQuality
         # update block info, update results file info
         # save fils
         # write log
-    def update_fields(self,new_rating,is_rated,is_interpolated):
-        self.rating = new_rating
-        self.is_rated = is_rated
-        self.is_interpolated = is_interpolated
-            
-    def create_results_file(self):
-        """
-        Takes object information needed to be saved and creates dictionary
-        
-        Parameters
-        ----------
-        none
-        
-        Returns
-        -------
-        results: dict
-            dictionary containing all of the relevant info needed to be saved
-        
-        """
-        results = vars(self)
-        return results
-        # need to decide which fields to not include in the saved file
-        # use pop() to remove them
+
         
