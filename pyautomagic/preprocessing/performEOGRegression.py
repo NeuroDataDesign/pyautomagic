91345c8179e61bf16d5185fff6b06e926f222c47
AamnaLawrence <aamna.lawrence@gmail.com>
2019-12-02 07:35:31 -0500

    Updated EOG Regression code

--- pyautomagic/preprocessing/performEOGRegression.py
+++ pyautomagic/preprocessing/performEOGRegression.py
@@ -1,44 +1,44 @@
 import numpy as np
-import logging
-import mne
 
-def performEOGRegression(raw):
-    """Performs linear regression to remove EOG artifact from the EEG data
+
+def perform_EOG_regression(EEG, EOG):
+    """The artifacts due to EOG activity are removed from the EEG data using the subtraction method
+        that relies on the linear transformation of the EEG signal
 
         Parameters
         ----------
-        raw: MNE raw data structure
+        EEG: np.ndarray
+             The input EEG data
+        EOG: np.ndarray
+             The input EOG data
 
         Returns
         -------
         clean_EEG: np.ndarray
                    Cleaned EEG signal from EOG artifacts
 
+        References
+        __________
+        [1] Pedroni, A., Bahreini, A., & Langer, N. (2019). Automagic: Standardized preprocessing of big EEG data.
+        Neuroimage, 200, 460-473. doi: 10.1016/j.neuroimage.2019.06.046
         """
 
-    EEG=raw.get_data()
-    EOG_idx=mne.pick_types(raw.info, eog=True)
-    EOG=EEG[EOG_idx]
-    # checks if EOG Regression should be skipped or not depending
-    # on whether EOG was recorded
-    if EOG_idx.shape[0] == 0:
-        logging.warning('EOG regression skipped')
-        return
-
-    size_EOG = EOG.shape
-    dim = len(size_EOG)
+    size_EEG = np.shape(EEG)
+    size_EOG = np.shape(EOG)
+    dimension1 = len(size_EOG)
+    dimension2 = len(size_EEG)
     # resizing the EOG array so that its pseudoinverse can be calculated
-    if dim == 1:
+    if dimension1 == 1:
         EOG.resize((1, size_EOG[0]))
+    if dimension2 == 1:
+        EEG.resize((1, size_EEG[0]))
+
     EEG_t = np.transpose(EEG)
     EOG_t = np.transpose(EOG)
     # performing pseudoinverse
     pseudoinv = np.linalg.pinv(np.dot(np.transpose(EOG_t), EOG_t))
-    inv = np.dot(pseudoinv,np.transpose(EOG_t))
+    inv = np.dot(pseudoinv, np.transpose(EOG_t))
     subtract_EOG = np.dot(EOG_t, np.dot(inv, EEG_t))
     # subtracting the EOG noise from the EEG signal
     clean_EEG = np.transpose(np.subtract(EEG_t, subtract_EOG))
     return clean_EEG
-
-
-
