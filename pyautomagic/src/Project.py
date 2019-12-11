510033a4e8bf20ae45824b1c6206cb68bf338b26
saulmezac <[smezaca1@jhu.edu]>
2019-12-11 15:16:48 -0500

    Updating Project file import statements

--- pyautomagic/src/Project.py
+++ pyautomagic/src/Project.py
@@ -16,6 +16,8 @@ import numpy as np
 import timeit
 import os
 import logging
+import mne
+import mne_bids
 from mne_bids.utils import _write_json
 from pyautomagic.src.rateQuality import rateQuality
 from pyautomagic.src.Block import Block
