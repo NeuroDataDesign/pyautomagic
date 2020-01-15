62ede930680e463c992cf341b7bbbbf4ea1bdd57
Adam Li <adam2392@gmail.com>
2020-01-15 18:14:09 -0500

    Adding updates to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -12,19 +12,27 @@
 #
 import os
 import sys
+import datetime
+import pyautomagic
+
+import sphinx_gallery  # noqa: F401
+from sphinx_gallery.sorting import ExampleTitleSortKey
+import sphinx_bootstrap_theme
+
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../pyautomagic/src'))
 sys.path.insert(0, os.path.abspath('../pyautomagic/preprocessing'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'pyatuomagic'
+project = u'pyautoomagic'
+td = datetime.date.today()
 copyright = '2019, C. Zurn, R. Bechtold, A. Lawrence, L. Xiang, S. Meza, D. Soni'
 author = 'C. Zurn, R. Bechtold, A. Lawrence, L. Xiang, S. Meza, D. Soni'
 
+version = pyautomagic.__version__
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
-
+release = version
 
 # -- General configuration ---------------------------------------------------
 
@@ -49,15 +57,57 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
+# HTML options (e.g., theme)
+# see: https://sphinx-bootstrap-theme.readthedocs.io/en/latest/README.html
+# Clean up sidebar: Do not show "Source" link
+html_show_sourcelink = False
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    'navbar_title': 'PyAutomagic',
+    'bootswatch_theme': "flatly",
+    'navbar_sidebarrel': False,  # no "previous / next" navigation
+    'navbar_pagenav': False,  # no "Page" navigation in sidebar
+    'bootstrap_version': "3",
+    'navbar_links': [
+        ("Examples", "auto_examples/index"),
+        ("API", "api"),
+        ("What's new", "whats_new"),
+        ("Github", "https://github.com/neurodatadesign/pyautomagic", True),
+    ]}
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'mne': ('https://mne.tools/stable/', None),
+    'mne-bids': ('http://mne.tools/mne-bids/', None),
+    'numpy': ('https://www.numpy.org/devdocs', None),
+    'scipy': ('https://scipy.github.io/devdocs', None),
+    'matplotlib': ('https://matplotlib.org', None),
+}
+
+sphinx_gallery_conf = {
+    'examples_dirs': '../examples',
+    'within_subsection_order': ExampleTitleSortKey,
+    'gallery_dirs': 'auto_examples',
+    'filename_pattern': '^((?!sgskip).)*$',
+    'backreferences_dir': 'generated',
+    'binder': {
+        # Required keys
+        'org': 'neurodatadesign',
+        'repo': 'pyautomagic',
+        'branch': 'gh-pages',  # noqa: E501 Can be any branch, tag, or commit hash. Use a branch that hosts your docs.
+        'binderhub_url': 'https://mybinder.org',  # noqa: E501 Any URL of a binderhub deployment. Must be full URL (e.g. https://mybinder.org).
+        'dependencies': [
+            './environment.yml'
+        ],
+    }
+}
 
 # -- Options for LaTeX output -------------------------------------------------
 
